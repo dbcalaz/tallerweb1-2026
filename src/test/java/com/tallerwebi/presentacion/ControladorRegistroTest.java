7d56ed237e1a393ef8ad3b49a36c012b1b7bc717
Juan Bringa <bringajmit@gmail.com>
2026-04-21 10:32:28 -0300

    Primer test funcional de controladorRegistroTest

--- src/test/java/com/tallerwebi/presentacion/ControladorRegistroTest.java
+++ src/test/java/com/tallerwebi/presentacion/ControladorRegistroTest.java
@@ -1,4 +1,40 @@
 package com.tallerwebi.presentacion;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalToIgnoringCase;
+
 public class ControladorRegistroTest {
+
+    private final String mail = "juan@mmm.com";
+    private final String password ="1234";
+
+    ControladorRegistro controladorRegistro = new ControladorRegistro();
+
+    @Test
+    public void siSeIngresaEmailYPasswordElRegistroEsExitoso(){
+
+        //Presentacion
+        givenNoExisteUsuario();
+        //ejecucion
+        ModelAndView modelAndView = whenRegistroUsuario(mail,password);
+        //comprobacion
+        thenElRegistroEsExitoso(modelAndView);
+
+    }
+
+    private void thenElRegistroEsExitoso(ModelAndView modelAndView) {
+        assertThat( modelAndView.getViewName(), equalToIgnoringCase( "login"));
+        assertThat(modelAndView.getModel().get("mensaje").toString(), equalToIgnoringCase("el registro fue exitoso"));
+    }
+
+    private ModelAndView whenRegistroUsuario(String mail, String password) {
+        ModelAndView mav = controladorRegistro.registrar(mail, password);
+        return mav;
+    }
+
+    private void givenNoExisteUsuario() {
+    }
 }
