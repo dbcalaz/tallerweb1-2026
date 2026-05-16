ff4dfba92fe3cc152d52ac75260842a385134e5f
Juan Bringa <bringajmit@gmail.com>
2026-05-16 14:28:43 -0300

    se crea rama solicitar viaje

--- src/main/java/com/tallerwebi/presentacion/ControladorSolicitarViaje.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorSolicitarViaje.java
@@ -1,17 +1,17 @@
 package com.tallerwebi.presentacion;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+//import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ControladorSolicitarViaje {
 
-  @RequestMapping(path = "/solicitarViaje")
+  @RequestMapping("/solicitarViaje")
   public ModelAndView solicitarViaje() {
-    ModelMap modelo = new ModelMap();
-    modelo.put("solicitud", modelo);
-    return new ModelAndView("solicitarViaje", modelo);
+    //    ModelMap modelo = new ModelMap();
+    //    modelo.put("solicitud", modelo);
+    return new ModelAndView("solicitarViaje");
   }
 }
