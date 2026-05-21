6946153ab0fd318e61a1a6058660165122b0aeda
javier <javi_a15@live.com.ar>
2026-05-21 02:00:05 -0300

    Resuelve conflicto de merge en la versión de lombok

--- src/main/java/com/tallerwebi/dominio/Combi.java
+++ src/main/java/com/tallerwebi/dominio/Combi.java
@@ -1,7 +1,6 @@
 package com.tallerwebi.dominio;
 
-import lombok.Getter;
-import lombok.Setter;
+
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,8 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-@Getter
-@Setter
+
 public class Combi {
 
 
@@ -22,5 +20,35 @@ public class Combi {
     private Integer cantidadDeAsientos;
     private String tipoDeTransmision;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public TipoDeCombi getTipoDeCombi() {
+        return tipoDeCombi;
+    }
+
+    public void setTipoDeCombi(TipoDeCombi tipoDeCombi) {
+        this.tipoDeCombi = tipoDeCombi;
+    }
+
+    public String getTipoDeTransmision() {
+        return tipoDeTransmision;
+    }
+
+    public void setTipoDeTransmision(String tipoDeTransmision) {
+        this.tipoDeTransmision = tipoDeTransmision;
+    }
+
+    public Integer getCantidadDeAsientos() {
+        return cantidadDeAsientos;
+    }
+
+    public void setCantidadDeAsientos(Integer cantidadDeAsientos) {
+        this.cantidadDeAsientos = cantidadDeAsientos;
+    }
 }
