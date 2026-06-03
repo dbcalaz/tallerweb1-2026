1c280315b399250e679a7e0d527ea75c49c09f59
Antonio Sandoval <sandoval.mas23@gmail.com>
2026-06-03 14:16:16 -0300

    Busqueda de viajes (actualizado)

--- src/main/java/com/tallerwebi/dominio/Viaje.java
+++ src/main/java/com/tallerwebi/dominio/Viaje.java
@@ -25,4 +25,6 @@ public class Viaje {
 
     @ManyToOne
     private Conductor conductor;
+    private String duracion;
+    private String tipoServicio;
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,8 @@
+SET FOREIGN_KEY_CHECKS = 0;
+TRUNCATE TABLE Viaje;
+TRUNCATE TABLE Usuario;
+SET FOREIGN_KEY_CHECKS = 1;
+
 INSERT INTO Usuario(id, email, password, rol, activo) VALUES(null, 'test@unlam.edu.ar', 'test', 'ADMIN', true);
-
-INSERT INTO Viaje (origen, destino, fecha, horario, precio, asientosDisponibles)
-VALUES ('San Justo', 'Ramos Mejia', '2026-05-29', '10:30', 4500.0, 4);
-
-INSERT INTO Viaje (origen, destino, fecha, horario, precio, asientosDisponibles)
-VALUES ('Moron', 'Moreno', '2026-05-30', '14:00', 3800.0, 2);
+INSERT INTO Viaje (origen, destino, fecha, horario, precio, asientosDisponibles, duracion, tipoServicio) VALUES ('San Justo', 'Ramos Mejia', '2026-06-29', '10:30', 4500.0, 4, '25 min', 'Combi Comun');
+INSERT INTO Viaje (origen, destino, fecha, horario, precio, asientosDisponibles, duracion, tipoServicio) VALUES ('Moron', 'Moreno', '2026-06-30', '14:00', 3800.0, 2, '1h 10 min', 'Semicama Ejecutivo');
