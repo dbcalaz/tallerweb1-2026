7e3375bb1427a53dafc3d5b2a654705cbcdb524e
dbcalaz <dbcalaz@gmail.com>
2026-06-15 13:06:20 -0300

    Cuando se registra un conductor, tiene que aprobarlo el administrador, entre otras cosas.

--- src/main/java/com/tallerwebi/dominio/ServicioAdministradorImpl.java
+++ src/main/java/com/tallerwebi/dominio/ServicioAdministradorImpl.java
@@ -36,4 +36,76 @@ public class ServicioAdministradorImpl implements ServicioAdministrador {
         return repositorioAdministrador.getCombis();
     }
 
+    @Override
+    public Long obtenerCantidadCombis() {
+        return repositorioAdministrador.getCantidadDeCombis();
+    }
+
+    @Override
+    public List<Conductor> obtenerConductoresPendientes() {
+        return repositorioAdministrador.getConductoresPendientes();
+    }
+
+    @Override
+    public Long obtenerCantidadDeConductoresPendientes() {
+        return repositorioAdministrador.getCantidadDeConductoresPendientes();
+    }
+
+    @Override
+    public void habilitarConductor(Long idConductor, Long idCombi) {
+
+        Conductor conductor = repositorioAdministrador.buscarConductorPorId(idConductor);
+
+        if(conductor == null){
+            throw new RuntimeException("No existe el conductor seleccionado");
+        }
+
+        Combi combi = repositorioAdministrador.buscarCombiPorId(idCombi);
+
+        if(combi == null){
+            throw new RuntimeException("No existe la combi seleccionada");
+        }
+
+        if(conductor.isSuspendido()){
+            throw new RuntimeException("El conductor está suspendido");
+        }
+
+        if(conductor.isCuentaHabilitada()){
+            throw new RuntimeException("El conductor ya fue habilitado");
+        }
+
+        conductor.setCuentaHabilitada(true);
+        repositorioAdministrador.actualizarConductor(conductor);
+
+        AsignacionCombiConductor asignacion = new AsignacionCombiConductor();
+
+        asignacion.setConductor(conductor);
+        asignacion.setCombi(combi);
+        asignacion.setCombiActiva(true);
+
+        repositorioAdministrador.guardarAsignacion(asignacion);
+    }
+
+    @Override
+    public void suspenderConductor(Long idConductor) {
+        Conductor conductor = repositorioAdministrador.buscarConductorPorId(idConductor);
+
+        if(conductor == null){
+            throw new RuntimeException("No existe el conductor seleccionado");
+        }
+
+        repositorioAdministrador.suspenderConductor(conductor);
+    }
+
+    @Override
+    public void reactivarConductor(Long idConductor) {
+        Conductor conductor = repositorioAdministrador.buscarConductorPorId(idConductor);
+
+        if(conductor == null){
+            throw new RuntimeException("No existe el conductor seleccionado");
+        }
+
+        repositorioAdministrador.reactivarConductor(conductor);
+    }
+
 }
